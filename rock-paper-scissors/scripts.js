094a54d71fe605417d1583af1d2b3932f973d6e3
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-05-15 20:57:58 +0530

    revisiting rock-apaer-scissors on rps-ui branch

--- rock-paper-scissors/scripts.js
+++ rock-paper-scissors/scripts.js
@@ -1,73 +1,145 @@
+const buttonSection = document.getElementById("primary-buttons");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const humanChoiceBoard = document.getElementById("human-choice");
+const compuChoiceBoard = document.getElementById("computer-choice");
+const humanScoreBoard = document.getElementById("human-score");
+const compuScoreBoard = document.getElementById("computer-score");
+const resultRound = document.getElementById("result-round");
+const resultGame = document.getElementById("result-game");
+const restartGame = document.getElementById("restart");
+let computerScore = 0;
+let humanScore = 0;
+let endflag = 0;
 
+
+// const humanHand = getHumanChoice();
+// const compuHand = getComputerChoice();
 
 const getComputerChoice = () => {
     const collection = ["rock","paper","scissors"];
-    const choice_hand = Math.floor(Math.random()*3);
-    return collection[choice_hand];
+    return collection[Math.floor(Math.random()*3)];
 }
 
-const getHumanChoice = () => {
-    const humanchoice = prompt("Play your hand: \n");
-    if(humanchoice.toLowerCase() === "rock"||humanchoice.toLowerCase() === "paper"||humanchoice.toLowerCase() === "scissors"){
-        return humanchoice.toLowerCase();
+const playRound = (humanChoice, computerChoice) => {
+    if(humanChoice === computerChoice){
+        resultRound.innerText = "It's a draw";
+        humanChoiceBoard.innerText = `You played: ${humanChoice}`;
+        compuChoiceBoard.innerText = `Computer played: ${computerChoice}`;
+        humanScoreBoard.innerText = `Your current score: ${humanScore}`;
+        compuScoreBoard.innerText = `Computer's current score: ${computerScore}`;
+        scorejudge(humanScore,computerScore);
     }
-    else{
-        alert("Please enter only 'rock', 'paper' or 'scissors'");
+    else if(humanChoice === 'rock' && computerChoice === 'paper'){
+        resultRound.innerText = "You lose! paper beats rock";
+        computerScore++;
+        humanChoiceBoard.innerText = `You played: ${humanChoice}`;
+        compuChoiceBoard.innerText = `Computer played: ${computerChoice}`;
+        humanScoreBoard.innerText = `Your current score: ${humanScore}`;
+        compuScoreBoard.innerText = `Computer's current score: ${computerScore}`;
+        scorejudge(humanScore,computerScore);
+    }
+    else if(humanChoice === 'paper' && computerChoice === 'scissors'){
+        resultRound.innerText = "You lose! scissors beats paper";
+        computerScore++;
+        humanChoiceBoard.innerText = `You played: ${humanChoice}`;
+        compuChoiceBoard.innerText = `Computer played: ${computerChoice}`;
+        humanScoreBoard.innerText = `Your current score: ${humanScore}`;
+        compuScoreBoard.innerText = `Computer's current score: ${computerScore}`;
+        scorejudge(humanScore,computerScore);
+    }
+    else if(humanChoice === 'scissors' && computerChoice === 'rock'){
+        resultRound.innerText = "You lose! rock beats scissors";
+        computerScore++;
+        humanChoiceBoard.innerText = `You played: ${humanChoice}`;
+        compuChoiceBoard.innerText = `Computer played: ${computerChoice}`;
+        humanScoreBoard.innerText = `Your current score: ${humanScore}`;
+        compuScoreBoard.innerText = `Computer's current score: ${computerScore}`;
+        scorejudge(humanScore,computerScore);
+    }
+    else if(humanChoice === 'rock' && computerChoice === 'scissors'){
+        resultRound.innerText = "You win! rock beats scissors";
+        humanScore++;
+        humanChoiceBoard.innerText = `You played: ${humanChoice}`;
+        compuChoiceBoard.innerText = `Computer played: ${computerChoice}`;
+        humanScoreBoard.innerText = `Your current score: ${humanScore}`;
+        compuScoreBoard.innerText = `Computer's current score: ${computerScore}`;
+        scorejudge(humanScore,computerScore);
+    }
+    else if(humanChoice === 'paper' && computerChoice === 'rock'){
+        resultRound.innerText = "You win! paper beats rock";
+        humanScore++;
+        humanChoiceBoard.innerText = `You played: ${humanChoice}`;
+        compuChoiceBoard.innerText = `Computer played: ${computerChoice}`;
+        humanScoreBoard.innerText = `Your current score: ${humanScore}`;
+        compuScoreBoard.innerText = `Computer's current score: ${computerScore}`;
+        scorejudge(humanScore,computerScore);
+    }
+    else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+        resultRound.innerText = "You win! scissors beats paper";
+        humanScore++;
+        humanChoiceBoard.innerText = `You played: ${humanChoice}`;
+        compuChoiceBoard.innerText = `Computer played: ${computerChoice}`;
+        humanScoreBoard.innerText = `Your current score: ${humanScore}`;
+        compuScoreBoard.innerText = `Computer's current score: ${computerScore}`;
+        scorejudge(humanScore,computerScore);
+    }
+}
+
+
+const playGame = (e) => {
+    const humanHand = e;
+    const compuHand = getComputerChoice();
+    playRound(humanHand, compuHand);
+}
+
+const scorejudge = (humanscore,computerscore) => {
+
+    if(humanscore > computerscore && humanscore === 5){
+        resultGame.innerText = `You Win! Your score is: ${humanScore} and Computer's score is: ${computerScore}`;
+        restartGame.style.display = "block";
+        buttonSection.style.display = "none";
+    }
+    else if(humanscore < computerscore && computerscore === 5){
+        resultGame.innerText = `Computer Wins! Your score is: ${humanScore} and Computer's score is: ${computerScore}`;
+        restartGame.style.display = "block";
+        buttonSection.style.display = "none";
+    }
+    else {
         return;
     }
 }
 
-
-const playGame = () => {
-    let computerScore = 0;
-    let humanScore = 0;
-    let numRounds = 0;
-    const playRound = (humanChoice, computerChoice) => {
-        if(humanChoice === computerChoice){
-            console.log("It's a draw \n");
-        }
-        else if(humanChoice === 'rock' && computerChoice === 'paper'){
-            console.log("You lose! paper beats rock \n");
-            computerScore++;
-        }
-        else if(humanChoice === 'paper' && computerChoice === 'scissors'){
-            console.log("You lose! scissors beats paper \n");
-            computerScore++;
-        }
-        else if(humanChoice === 'scissors' && computerChoice === 'rock'){
-            console.log("You lose! rock beats scissors \n");
-            computerScore++;
-        }
-        else if(humanChoice === 'rock' && computerChoice === 'scissors'){
-            console.log("You win! rock beats scissors \n");
-            humanScore++;
-        }
-        else if(humanChoice === 'paper' && computerChoice === 'rock'){
-            console.log("You win! paper beats rock \n");
-            humanScore++;
-        }
-        else if(humanChoice === 'scissors' && computerChoice === 'paper'){
-            console.log("You win! scissors beats paper \n");
-            humanScore++;
-        }
-    }
-    for(let i=0; i < 5; i++){
-        const humanHand = getHumanChoice();
-        const compuHand = getComputerChoice();
-        playRound(humanHand, compuHand);
-    }
-    if(humanScore > computerScore){
-        console.log("You Win! \n","Your score: " + humanScore + " ", "Computer's score: " + computerScore + " ");
-    }
-    else if(humanScore < computerScore){
-        console.log("Computer Wins! \n","Your score: " + humanScore + " ", "Computer's score: " + computerScore + " ");
-    }
-    else{
-        console.log("It's a tie. \n")
-    }
+const gameReset = () => {
+    humanChoiceBoard.innerText = ""
+    compuChoiceBoard.innerText = ""
+    humanScoreBoard.innerText = ""
+    compuScoreBoard.innerText = ""
+    resultRound.innerText = ""
+    resultGame.innerText = ""
+    computerScore = 0;
+    humanScore = 0;
+    restartGame.style.display = "none";
+    buttonSection.style.display = "flex";
 }
 
-window.onload = playGame();
+rock.addEventListener("click",() => {
+    playGame("rock");
+})
+
+paper.addEventListener("click",() => {
+    playGame("paper");
+})
+
+scissors.addEventListener("click",() => {
+    playGame("scissors");
+})
+
+restartGame.addEventListener("click",gameReset);
+//window.onload = playGame();
 
 //console.log(getComputerChoice());
 //console.log(getHumanChoice());
+
+
